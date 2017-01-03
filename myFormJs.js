d1cd527e6a97ba4dfd2bb1ed51f882e1f42b7371
Pooja <pthakare33@gmail.com>
2017-01-03 14:20:27 -0800

    sql implementation

--- myFormJs.js
+++ myFormJs.js
@@ -1,6 +1,6 @@
-var adminUrl="http://localhost:1337";
+var adminUrl = "http://localhost:1337";
 angular.module('BlankApp', ['ngMaterial'])
-    .controller('AppCtrl', function ($scope, $http) {
+    .controller('AppCtrl', function ($scope, $http, $filter) {
         $scope.myDate = new Date();
         $scope.user = {};
         $scope.minDate = new Date(
@@ -18,8 +18,26 @@ angular.module('BlankApp', ['ngMaterial'])
             var day = date.getDay();
             return day === 0 || day === 6;
         };
+        format = function date2str(x, y) {
+            var z = {
+                M: x.getMonth() + 1,
+                d: x.getDate(),
+                h: x.getHours(),
+                m: x.getMinutes(),
+                s: x.getSeconds()
+            };
+            y = y.replace(/(M+|d+|h+|m+|s+)/g, function (v) {
+                return ((v.length > 1 ? "0" : "") + eval('z.' + v.slice(-1))).slice(-2)
+            });
+
+            return y.replace(/(y+)/g, function (v) {
+                return x.getFullYear().toString().slice(-v.length)
+            });
+        }
         $scope.submitForm = function (user) {
-            console.log(user);
+            $scope.user = user;
+            $scope.user.dob = $filter('date')(Date.parse($scope.user.dob), 'yyyy-MM-dd');
+            console.log($scope.user);
             $http.post("http://localhost:3000/view1", user).success(function (data, status) {
                 result = data;
             }).error(function () {
